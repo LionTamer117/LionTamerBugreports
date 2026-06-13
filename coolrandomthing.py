693ecb723e8ec1897a69365d498ee4f2e4b337ff
LionTamer117 <154023976+LionTamer117@users.noreply.github.com>
2026-06-13 16:29:31 -0500

    Update coolrandomthing.py

--- coolrandomthing.py
+++ coolrandomthing.py
@@ -1,1 +1,15 @@
-print("this could be a leak")
+NPC_name = "[Bob]"
+Username = input(f"{NPC_name} Hello there! What is ur name? ")
+print(NPC_name, "Oh that sounds so cool:", Username)
+NPC_name_change = input("[!] Come up with your own name for the npc! Here: ")
+NPC_name = f"[{NPC_name_change}]"
+print("[!] The NPC is now named:", NPC_name)
+print(f"{NPC_name} Hello again! it seems that i have no friends may you help?")
+NPC2_name_make = input("[!] Come up with your own name for the npc! Here: ")
+NPC2_name = f"[{NPC_name_change}]"
+print("[!] The 2nd NPC is now named:", NPC2_name)
+print(f"{NPC_name} Thank you so much! Now i have my friend", NPC2_name)
+#now thats pretty cool
+print("scalded boss in acidic biome? must be a leak.")
+#OK THATS PEAK LEAK RIGHT THERE
+#yes
